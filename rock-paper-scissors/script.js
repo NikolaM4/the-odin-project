6b24e2508605a639a36629bd09ab2c9c85f25b04
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-01-08 20:46:58 +0100

    Finished playRound function

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -5,6 +5,19 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  if (playerSelection == "rock")
+    if (computerSelection == "scissors") return "You win! Rock beats Scissors";
+    else if (computerSelection == "paper") return "You Lose! Paper beats Rock";
+    else return "It's draw";
+  else if (playerSelection == "paper")
+    if (computerSelection == "scissors") return "You Lose! Scissors beats Paper";
+    else if (computerSelection == "rock") return "You win! Paper beats Rock";
+    else return "It's draw";
+  else if (playerSelection == "scissors")
+    if (computerSelection == "rock") return "You Lose! Rock beats Scissors";
+    else if (computerSelection == "paper") return "You win! Scissors beats Paper";
+    else return "It's draw";
   return;
 }
 
