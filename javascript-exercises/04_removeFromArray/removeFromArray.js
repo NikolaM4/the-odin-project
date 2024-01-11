af47fb5d335ede5703ff0ac24cca22e3798d8cc8
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-01-11 15:40:34 +0100

    Finished 04_removeFromArray with passing all tests

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...elements) {
+  let elementsLength = elements.length;
+  let positionOfElementInArray = 0;
+  for (let i = 0; i < elementsLength; i++) {
+    if (array.indexOf(elements[i]) != -1) {
+      positionOfElementInArray = array.indexOf(elements[i]);
+      array.splice(positionOfElementInArray, 1);
+    }
+  }
+  return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
