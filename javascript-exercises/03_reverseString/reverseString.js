67b1e11e97d744522933579438bccee5cc9409fe
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-01-11 15:19:38 +0100

    Finished exercise 03_reverseString with passing all tests

--- javascript-exercises/03_reverseString/reverseString.js
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reversedString = "";
+  let stringLength = string.length;
+  for (let i = stringLength - 1; i >= 0; i--) reversedString += string[i];
+  return reversedString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
