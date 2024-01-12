a7dea2ff461fb3acaf354aaee9c72dcbd4bd4497
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-01-12 13:41:47 +0100

    Finished 05_summAll with passing all tests

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (numberBegin, numberEnd) {
+  if (typeof numberBegin != "number" || typeof numberEnd != "number" || numberBegin < 0 || numberEnd < 0) return "ERROR";
+  let sum = 0;
+  if (numberBegin <= numberEnd) for (let i = numberBegin; i <= numberEnd; i++) sum += i;
+  else for (let i = numberBegin; i >= numberEnd; i--) sum += i;
+  return sum;
 };
 
 // Do not edit below this line
