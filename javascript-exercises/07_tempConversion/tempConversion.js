25d7507b004ec2cbbcbe4cb16c76b5046f8ca15d
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-01-12 13:53:34 +0100

    Finished 07_tempConversion with passing all tests

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInFahr) {
+  return Math.round((tempInFahr - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInCels) {
+  return Math.round((tempInCels * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
