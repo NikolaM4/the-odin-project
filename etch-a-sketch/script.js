3b7e87113c2a0c874e47720e394b41f02f682f55
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-01-12 16:40:07 +0100

    Created function createGrid for generating grid of given size

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,10 +1,30 @@
-let divContainer = document.querySelector(".container");
+const SIZE_OF_DRAWING_FIELD = 800;
 
-for (let i = 0; i < 16; i++)
-  for (let j = 0; j < 16; j++) {
-    let newDiv = document.createElement("div");
-    newDiv.style.width = "20px";
-    newDiv.style.height = "20px";
-    newDiv.style.border = "1px solid rgba(0,0,0, 0.1";
-    divContainer.appendChild(newDiv);
-  }
+let divContainer = document.querySelector(".container");
+let buttonChangeGrid = document.querySelector("#btnChangeGrid");
+
+function createGrid(gridRows) {
+  let sizeOfCell = SIZE_OF_DRAWING_FIELD / gridRows;
+  for (let i = 0; i < gridRows; i++)
+    for (let j = 0; j < gridRows; j++) {
+      let newDiv = document.createElement("div");
+      newDiv.style.width = `${sizeOfCell - 2}px`;
+      newDiv.style.height = `${sizeOfCell - 2}px`;
+      newDiv.style.border = "1px solid rgba(0,0,0, 0.1)";
+      newDiv.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "black";
+      });
+      divContainer.appendChild(newDiv);
+    }
+}
+
+let gridRows = 64;
+createGrid(gridRows);
+
+buttonChangeGrid.addEventListener("click", (e) => {
+  gridRows = parseInt(prompt("How many squares per side do you want?"));
+
+  // while (gridRows < 1 || gridRows >= 100 || typeof rows != "number") {
+  //   gridRows = prompt("You can't enter that. Type number:");
+  // }
+});
