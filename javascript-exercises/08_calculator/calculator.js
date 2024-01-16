2b050a11b6e0134f00d43146f51c87e1858a25f7
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-01-16 13:50:24 +0100

    Finished 08_calculator with passing all tests

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let sumOfNumbers = 0;
+  if (numbers.length == 0) return sumOfNumbers;
+  numbers.forEach((number) => {
+    sumOfNumbers += number;
+  });
+  return sumOfNumbers;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  let product = 1;
+  numbers.forEach((number) => {
+    product *= number;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, n) {
+  return Math.pow(a, n);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number == 0 || number == 1) return 1;
+  else return number * factorial(number - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
