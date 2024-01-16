4e34e4078854513897ef80e03e6839873f2d97dc
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-01-16 14:09:29 +0100

    Finished 09_palindromes with passing all tests

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let newString = string
+    .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "")
+    .toLowerCase()
+    .replaceAll(" ", "");
+  let reversedString = newString.split("").reverse().join("");
+  return newString === reversedString;
 };
 
 // Do not edit below this line
