3a4950dacacf71d4e82f5e7c7786caf800924af6
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-01-16 14:19:21 +0100

    Finished 10_fibonacci with passing all tests

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (number < 0) return "OOPS";
+  let fibonacciSum;
+  let num1 = 1,
+    num2 = 1;
+  if (+number == 0) return 0;
+  else if (+number == 1) return 1;
+  else if (+number == 2) return 1;
+  for (let i = 3; i <= +number; i++) {
+    fibonacciSum = num1 + num2;
+    num1 = num2;
+    num2 = fibonacciSum;
+  }
+  return fibonacciSum;
 };
 
 // Do not edit below this line
