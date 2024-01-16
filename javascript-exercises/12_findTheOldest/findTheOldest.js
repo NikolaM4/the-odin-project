aebf1d0a7884552c3f591dfc79b9cbebfac356d2
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-01-16 14:40:39 +0100

    Finished 12_findTheOldest with passing all tests

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let currentYear = 2024;
+  let highestAge = 0;
+  let oldestPerson = { name: people[0].name };
+  if (people[0].yearOfDeath != undefined) {
+    highestAge = people[0].yearOfDeath - people[0].yearOfBirth;
+    oldestPerson.name = people[0].name;
+  } else {
+    highestAge = currentYear - people[0].yearOfBirth;
+    oldestPerson.name = people[0].name;
+  }
+  people.map((person) => {
+    if (person.yearOfDeath != undefined) {
+      if (person.yearOfDeath - person.yearOfBirth > highestAge) {
+        highestAge = person.yearOfDeath - person.yearOfBirth;
+        oldestPerson.name = person.name;
+      }
+    } else if (currentYear - person.yearOfBirth > highestAge) {
+      highestAge = currentYear - person.yearOfBirth;
+      oldestPerson.name = person.name;
+    }
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
