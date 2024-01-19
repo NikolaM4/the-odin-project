2490c8e633a23a0241a10e8701ac1d93f6811a8d
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-01-19 17:28:01 +0100

    Adding elementary HTML, a little CSS and initialization of JS functions

---
+++ calculator/script.js
@@ -0,0 +1,20 @@
+function add() {}
+
+function substract() {}
+
+function multiply() {}
+
+function divide() {}
+
+let number1;
+let number2;
+let operator;
+
+function operate(number1, operator, number2) {}
+
+let displayValue = 0;
+let display = document.querySelector("#display");
+
+function displayResult() {
+  display.innerHTML = displayValue;
+}
