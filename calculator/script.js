5bc658edadc0e6296cbb3f0df49bbcfe1f99070e
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-01-20 18:34:03 +0100

    Slighly more work on adding event listeners. Not working yet

--- calculator/script.js
+++ calculator/script.js
@@ -25,24 +25,64 @@ function operate(number1, operator, number2) {
   else if (operator == "/") divide(number1, number2);
 }
 
-let displayValue = 0;
+let displayValue = "0";
 let display = document.querySelector("#display");
 number1 = displayValue;
 
 let buttons = document.querySelectorAll(".calculatorButton");
 let currentButton;
 let numberOfOperators = 0;
+let numberOfDots = 0;
+let currentOperator;
 
 buttons.forEach((button) => {
   if (button.id == "button_=")
     button.addEventListener("click", () => {
       (displayValue = "gdshgds"), displayResult();
     });
-  else if ((button.id = "button_C"))
+  else if (button.id == "button_C") {
     button.addEventListener("click", () => {
       displayValue = 0;
       display.innerHTML = displayValue;
     });
+  } else if (button.id == "button_+") {
+    button.addEventListener("click", () => {
+      if (numberOfOperators == 1) displayValue = displayValue.slice(0, -1);
+      currentOperator = "+";
+      displayValue += currentOperator;
+      numberOfOperators = 1;
+      displayResult();
+    });
+  } else if (button.id == "button_-") {
+    button.addEventListener("click", () => {
+      console.log("idemo");
+      if (numberOfOperators == 1) displayValue = displayValue.slice(0, -1);
+      currentOperator = "-";
+      displayValue += currentOperator;
+      numberOfOperators = 1;
+      displayResult();
+    });
+  } else if (button.id == "button_*") {
+    button.addEventListener("click", () => {
+      if (numberOfOperators == 1) displayValue = displayValue.slice(0, -1);
+      currentOperator = "*";
+      displayValue += currentOperator;
+      numberOfOperators = 1;
+      displayResult();
+    });
+  } else if (button.id == "button_/") {
+    button.addEventListener("click", () => {
+      if (numberOfOperators == 1) currentOperator = "/";
+      displayValue += currentOperator;
+      numberOfOperators = 1;
+      displayResult();
+    });
+  } else {
+    button.addEventListener("click", () => {
+      displayValue += button.innerHTML;
+      displayResult();
+    });
+  }
 });
 
 function displayResult() {
